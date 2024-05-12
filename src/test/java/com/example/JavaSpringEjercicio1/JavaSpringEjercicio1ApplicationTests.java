085c74a8e711af92dcce6878302b87fa4bc6dd93
Marcos Chuy <Vicente194591@gmail.com>
2024-05-12 13:35:56 -0600

    Initial commit

---
+++ src/test/java/com/example/JavaSpringEjercicio1/JavaSpringEjercicio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaSpringEjercicio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringEjercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
